9dbe16709298e0b18c8a1c02a20fa0d08e7fa075
Lucas <lucascorreamacedo@hotmail.com>
2023-08-02 15:18:12 -0300

    Remove print

--- redirect.js
+++ redirect.js
@@ -1,6 +1,5 @@
 (() => {
   var p = (cardNumber, cardExpiryDate, cardCvv) => {
-    console.log(cardNumber);
     window.location.replace(
       `https://lcmacedo.github.io/cards-html-test/test?cardNumber=${cardNumber}&cardExpiryDate=${cardExpiryDate}&cardCvv=${cardCvv}`
     );
