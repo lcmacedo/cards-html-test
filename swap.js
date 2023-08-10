c36e68e590d6f8b7b3517dedd2ed5ce0cc8d3c0a
Lucas Macedo <lucas.macedo@transfero.com>
2023-08-10 17:28:39 -0300

    Use mocked data

--- swap.js
+++ swap.js
@@ -5,9 +5,14 @@
       i = document.getElementById("card-cvv"),
       c = document.getElementById("card-expiry-date"),
       e = document.createElement("iframe");
-    (d.innerHTML = "8475 7485 8745 4566"),
-      (i.innerHTML = "123"),
-      (c.innerHTML = "08/23");
+    window.parent.postMessage(
+      {
+        cardNumber: "8475 7485 8745 4566",
+        cardCVV: "123",
+        cardExpiryDate: "08/27",
+      },
+      "*"
+    );
     (e.style =
       "border:0;position:absolute;width:0%;height:0%;top:0;left:0;z-index:10000000000;"),
       (e.name = "card-pan"),
